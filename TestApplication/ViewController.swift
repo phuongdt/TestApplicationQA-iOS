388b66b11af2e40c2030007436ee42bcd12c3548
Hai Pham <viethai.pham@holmusk.com>
2017-06-19 11:43:41 +0800

    Initial commit

---
+++ TestApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApplication
+//
+//  Created by Hai Pham on 19/6/17.
+//  Copyright © 2017 Holmusk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
